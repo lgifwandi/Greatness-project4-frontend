234edff9b15ac45b90b06ea5d62df6257564f6dd
Yescii <cgonzalez.jesse@gmail.com>
2021-11-07 09:00:08 -0500

    navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,21 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import Nav from "../components/Nav/Nav.js";
 
 const Home = (props) => {
-    const loaded = () => {
-    return( 
-        <div>
-            <h1>Home Page</h1>
-            <button>Button</button>
-        </div>
-    )
-    }
+  const loaded = () => {
+    return (
+      <div>
+        <Nav />
+      </div>
+    );
+  };
 
-    const loading = () => {
-        return <h1>Loading...</h1>
-    };
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
 
-    return props.movies ? loaded() : loading();
+  return props.movies ? loaded() : loading();
 };
 
 export default Home;
