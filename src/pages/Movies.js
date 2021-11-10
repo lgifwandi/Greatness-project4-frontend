87abdcca8dc02a42dab3bbb2ce7caa76542da242
onepullnixxa <briseno.chris@gmail.com>
2021-11-09 19:47:42 -0500

    adds class names to Movies.js page

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -2,36 +2,44 @@ import { useParams } from "react-router";
 import WatchedForm from "../components/WatchedForm";
 import UpdateMovieForm from "../components/UpdateMovieForm";
 
-const Movies = ({movies, handleAddWatched, handleDeleteMovie, handleMovieUpdate}) => {   
-    const {id} = useParams();
-    let movie = []
-    console.log(movies);
-    
-    {movies.map((item) => {
-        if(item.id == id) {
-        return movie = item
-        }
-    })}
-   
-    return(
-        <div className="show-div">
-            <img className="image-show" src={movie.image} alt={movie.title}/>
-            <div className="show-text">
-            <h1>{movie.title}</h1>
-            <h3>{movie.description}</h3>
-            <h3>{movie.movie_rating}</h3>
-            <h3>{movie.release_date}</h3>
-            <h3>{movie.genre}</h3>
-            
+const Movies = ({
+  movies,
+  handleAddWatched,
+  handleDeleteMovie,
+  handleMovieUpdate,
+}) => {
+  const { id } = useParams();
+  let movie = [];
+  console.log(movies);
 
-           <WatchedForm id={id}handleAddWatched={handleAddWatched}/>
+  {
+    movies.map((item) => {
+      if (item.id == id) {
+        return (movie = item);
+      }
+    });
+  }
 
-           <UpdateMovieForm movie={movie} handleMovieUpdate={handleMovieUpdate}/>
+  return (
+    <div className="show-div">
+      <img className="image-show" src={movie.image} alt={movie.title} />
+      <div className="show-text">
+        <h1 className="movie-title">{movie.title}</h1>
+        <h3 className="movie-desc">{movie.description}</h3>
+        <h3 className="movie-rating">{movie.movie_rating}</h3>
+        <h3 className="movie-release">{movie.release_date}</h3>
+        <h3 className="movie-genre">{movie.genre}</h3>
 
-           <button onClick={() => handleDeleteMovie(movie.id)} >Delete Movie</button>
-           </div>
-        </div>
-    )
+        <WatchedForm id={id} handleAddWatched={handleAddWatched} />
+
+        <UpdateMovieForm movie={movie} handleMovieUpdate={handleMovieUpdate} />
+
+        <button onClick={() => handleDeleteMovie(movie.id)}>
+          Delete Movie
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Movies;
